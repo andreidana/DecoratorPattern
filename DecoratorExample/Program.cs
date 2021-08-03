df7e851a4a1309a0ce452c5d458b639e4441aa79
Andrei Dana <andrei.dana87@gmail.com>
2021-08-03 22:05:43 +0300

    Small refactoring

--- DecoratorExample/Program.cs
+++ DecoratorExample/Program.cs
@@ -4,13 +4,13 @@ namespace DecoratorExample
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             ISport footballer = new Footballer();
             Console.WriteLine($"A footballer runs with {footballer.Run()} km/h");
 
-            ISport kepper = new Keeper();
-            Console.WriteLine($"A keeper runs with {kepper.Run()} km/h");
+            ISport keeper = new Keeper();
+            Console.WriteLine($"A keeper runs with {keeper.Run()} km/h");
 
             ISport fieldPlayer = new FieldPlayer();
             Console.WriteLine($"A fieldPlayer runs with {fieldPlayer.Run()} km/h");
